8e20769b0ec0f503ade008565ebf2c5f5b8e2b85
Aravind Nair <aravi.it011@gmail.com>
2022-01-06 23:24:24 +0100

    Updated the train_reconstructor file

--- train_reconstructor.py
+++ train_reconstructor.py
@@ -4,6 +4,7 @@ import numpy as np
 from pathlib import Path
 import torch
 from PIL import Image
+from torchvision import transforms
 
 @click.command()
 @click.option("--num_batches", type=int, required=True)
@@ -15,25 +16,6 @@ from PIL import Image
 @click.option("--downsampling", type=int, default=4)
 @click.option("--num_adapter_units", type=int, default=16)
 @click.option("--device", type=str, default="cpu")
-
-def parse_data():
-    labels= []
-    images= []
-    examples_path = Path.cwd() / "examples"
-
-    with open(examples_path / "noise.npy", "rb") as noise_file:
-        noise_vectors = torch.from_numpy(np.load(noise_file)).to('cpu')
-
-    for counter in range(6125):
-        print(counter)
-        labels.append(noise_vectors[counter])
-
-        img = Image.open(examples_path+'image_'+str(counter)+'.png')
-        imarray = np.array(img, dtype=np.double)/255
-        images.append(imarray)
-
-    return images,labels
-    
 
 def main(
     num_batches: int,
@@ -56,13 +38,37 @@ def main(
     ).to(device)
     optimizer = torch.optim.Adam(reconstructor.parameters(), lr=learning_rate)
 
+    train_labels= []
+    train_images= []
+
+    mean = torch.tensor([0.485, 0.456, 0.406], dtype=torch.float32)
+    std = torch.tensor([0.229, 0.224, 0.225], dtype=torch.float32)
+
+    transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std)
+    ])
+    
     examples_path = Path.cwd() / "examples"
+    img_path = '/Users/aanair/Library/Caches/pypoetry/virtualenvs/kth-scalable-antigan-OYKsXUJ2-py3.9/.guild/runs/14ecd370b97e4a2abe3cb8c7377d6009/'
     with open(examples_path / "noise.npy", "rb") as noise_file:
         noise_vectors = torch.from_numpy(np.load(noise_file)).to(device)
     
-    train_images, train_labels = parse_data()
+    for counter in range(6125):
+        train_labels.append(noise_vectors[counter])
+        
+        img = Image.open(img_path+'image_'+str(counter)+'.png')
+        
+        imarray = np.array(img, dtype=np.double)/255
+        train_images.append(imarray)
 
-    
+    train_dataloader = torch.utils.data.DataLoader(
+    dataset=train_images,
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=2
+)
+    tensor_train_images = torch.tensor([train_images[i] for i in train], dtype=torch.float32, device=device)
 
     print(len(train_labels))
     print(len(train_images))
