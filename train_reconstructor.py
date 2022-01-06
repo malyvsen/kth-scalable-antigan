059360ae727df513a9021a412f600ccebe6e2a93
Aravind Nair <aravi.it011@gmail.com>
2022-01-06 18:47:05 +0100

    Commit to get latest version#

--- train_reconstructor.py
+++ train_reconstructor.py
@@ -3,7 +3,7 @@ import click
 import numpy as np
 from pathlib import Path
 import torch
-
+from PIL import Image
 
 @click.command()
 @click.option("--num_batches", type=int, required=True)
@@ -15,6 +15,26 @@ import torch
 @click.option("--downsampling", type=int, default=4)
 @click.option("--dense_width", type=int, default=256)
 @click.option("--device", type=str, default="cpu")
+
+def parse_data():
+    labels= []
+    images= []
+    examples_path = Path.cwd() / "examples"
+
+    with open(examples_path / "noise.npy", "rb") as noise_file:
+        noise_vectors = torch.from_numpy(np.load(noise_file)).to('cpu')
+
+    for counter in range(6125):
+        print(counter)
+        labels.append(noise_vectors[counter])
+
+        img = Image.open(examples_path+'image_'+str(counter)+'.png')
+        imarray = np.array(img, dtype=np.double)/255
+        images.append(imarray)
+
+    return images,labels
+    
+
 def main(
     num_batches: int,
     batch_size: int,
@@ -39,8 +59,12 @@ def main(
     examples_path = Path.cwd() / "examples"
     with open(examples_path / "noise.npy", "rb") as noise_file:
         noise_vectors = torch.from_numpy(np.load(noise_file)).to(device)
-        print(type(noise_vectors))
-    # TODO: iterate over the data and train the reconstructor
+    
+    train_images, train_labels = parse_data()
 
+    
+
+    print(len(train_labels))
+    print(len(train_images))
 
 main()
