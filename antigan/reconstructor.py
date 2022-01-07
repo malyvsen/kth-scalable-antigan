72e7b1fdca3e7567329145d7ba543da71374f327
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-01-07 21:16:32 +0100

    improve: remove adapters from reconstructor

--- antigan/reconstructor.py
+++ antigan/reconstructor.py
@@ -11,7 +11,6 @@ class Reconstructor(torch.nn.Module):
         num_conv_channels: int,
         kernel_size: int,
         downsampling: int,
-        num_adapter_units: int,
     ):
         super().__init__()
         self.convolutional = torch.nn.Sequential(
@@ -31,29 +30,10 @@ class Reconstructor(torch.nn.Module):
             ]
         )
         final_image_size = 512 // downsampling ** num_conv_layers
-        self.adapters = torch.nn.ModuleList(
-            torch.nn.Sequential(
-                torch.nn.Linear(final_image_size ** 2, num_adapter_units),
-                torch.nn.LeakyReLU(),
-            )
-            for channel in range(num_conv_channels)
-        )
         self.head = torch.nn.Linear(
-            num_conv_channels * num_adapter_units, noise_dimensionality
+            final_image_size ** 2 * num_conv_channels, noise_dimensionality
         )
 
     def forward(self, image):
-        convolved: torch.Tensor = self.convolutional(
-            image
-        )  # batch, channels, height, width
-        to_adapt = convolved.flatten(start_dim=2).permute(
-            1, 0, 2
-        )  # channels, batch, height * width
-        adapted = torch.stack(
-            [
-                adapter(channel_slice)
-                for channel_slice, adapter in zip(to_adapt, self.adapters)
-            ],
-            dim=1,
-        )  # batch, channels, num_adapter_units
-        return self.head(adapted.flatten(start_dim=1))
+        convolved = self.convolutional(image)
+        return self.head(convolved.flatten(start_dim=1))
