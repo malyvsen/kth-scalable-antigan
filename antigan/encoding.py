e88804638ef5449aaef48163984c45bf6c4cfc38
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-01-07 22:17:47 +0100

    refactor: split up encoding and app

---
+++ antigan/encoding.py
@@ -0,0 +1,52 @@
+import math
+import numpy as np
+import string
+from .config import noise_intensity, noise_dimensionality
+
+
+def text_to_noise(text: str) -> np.ndarray:
+    padded_text = text + " " * (max_text_characters - len(text))
+    noise_bits = [
+        bit for character in padded_text for bit in character_to_noise(character)
+    ]
+    return np.array(
+        noise_bits + [0] * (noise_dimensionality - len(noise_bits)),
+        dtype=np.float32,
+    )
+
+
+def character_to_noise(character: str):
+    assert len(character) == 1
+    index = allowed_characters.find(character)
+    assert index >= 0
+    binary = bin(index)[2:]
+    padded_binary = "0" * (bits_per_character - len(binary)) + binary
+    return [
+        -noise_intensity if bit == "0" else noise_intensity for bit in padded_binary
+    ]
+
+
+def noise_to_text(noise: np.ndarray) -> str:
+    return "".join(
+        noise_to_character(character_noise)
+        for character_noise in np.split(
+            noise[: max_text_characters * bits_per_character], max_text_characters
+        )
+    ).strip()
+
+
+def noise_to_character(noise: np.ndarray):
+    assert len(noise) == bits_per_character
+    binary = "".join("0" if value < 0 else "1" for value in noise)
+    index = int(binary, 2)
+    try:
+        return allowed_characters[index]
+    except IndexError:
+        return "?"
+
+
+allowed_characters = (
+    string.digits + string.ascii_letters + string.punctuation + string.whitespace
+)
+bits_per_character = math.ceil(math.log2(len(allowed_characters)))
+max_text_characters = noise_dimensionality // bits_per_character
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from antigan import Generator, config
-import math
-import numpy as np
+from antigan.encoding import (
+    text_to_noise,
+    noise_to_text,
+    allowed_characters,
+    max_text_characters,
+)
 import pickle
 from PIL import Image
 import streamlit as st
-import string
 import torch
 import torchvision as tv
 
@@ -27,19 +30,11 @@ def generate():
     if any(character not in allowed_characters for character in text):
         st.error("Only the following characters are allowed: " + allowed_characters)
         return
-    padded_text = text + " " * (max_text_characters - len(text))
-    noise_bits = [
-        bit for character in padded_text for bit in character_to_noise(character)
-    ]
-    noise_vector = torch.tensor(
-        noise_bits + [0] * (config.noise_dimensionality - len(noise_bits)),
-        dtype=torch.float32,
-    )
 
     generating_message = st.empty()
     generating_message.info("Generating, this might take a while")
     generator = load_generator()
-    image = generator.generate(noise_vector)
+    image = generator.generate(text_to_noise(text))
     generating_message.empty()
     st.image(image, caption="This image contains your message. Share wisely!")
 
@@ -58,43 +53,8 @@ def reconstruct():
         .detach()
         .numpy()
     )
-    text = "".join(
-        noise_to_character(character_noise)
-        for character_noise in np.split(
-            noise[: max_text_characters * bits_per_character], max_text_characters
-        )
-    )
     decoding_message.empty()
-    st.write(f"The hidden text is: {text.strip()}")
-
-
-def character_to_noise(character: str):
-    assert len(character) == 1
-    index = allowed_characters.find(character)
-    assert index >= 0
-    binary = bin(index)[2:]
-    padded_binary = "0" * (bits_per_character - len(binary)) + binary
-    return [
-        -config.noise_intensity if bit == "0" else config.noise_intensity
-        for bit in padded_binary
-    ]
-
-
-def noise_to_character(noise: np.ndarray):
-    assert len(noise) == bits_per_character
-    binary = "".join("0" if value < 0 else "1" for value in noise)
-    index = int(binary, 2)
-    try:
-        return allowed_characters[index]
-    except IndexError:
-        return "?"
-
-
-allowed_characters = (
-    string.digits + string.ascii_letters + string.punctuation + string.whitespace
-)
-bits_per_character = math.ceil(math.log2(len(allowed_characters)))
-max_text_characters = config.noise_dimensionality // bits_per_character
+    st.write(f"The hidden text is: {noise_to_text(noise)}")
 
 
 @st.cache(show_spinner=False, allow_output_mutation=True)
